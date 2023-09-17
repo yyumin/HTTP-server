4491c94b6fca151d61a8ed5a35074f6585e7f91c
yyumin <121361233+yyumin@users.noreply.github.com>
2023-09-16 17:13:50 -0700

    Add files via upload

---
+++ node-yumin/index.js
@@ -0,0 +1,61 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+  //extension of file
+  let extname = path.extname(filePath);
+
+  let contentType = "text/html";
+
+  //check ext and set content type
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        //page not found
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        //some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      //success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 5001;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
